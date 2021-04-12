3fcec96a1805dc8a4be690b435ac969e376d84dd
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-04-12 15:49:34 -0700

    working with api data

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,16 +1,19 @@
 import  { useState, useEffect } from 'react'
 import axios from 'axios'
  const Restaurants = () => {
+     const [restaurants, setRestaurants] = useState([])
      useEffect(() =>{
         async function getApi(){
             const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/restaurants`)
             console.log(response.data)
+            const jsonData = response.data
+            setRestaurants(jsonData)
         }
         getApi()
      }, [])
      return(
          <div>
-
+             <li className="rest">{restaurants.restaurant_name}</li>
          </div>
      )
  }
